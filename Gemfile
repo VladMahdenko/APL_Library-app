180190f04ca4272a3a17e7d3df6779a7b511c743
VladMahdenko <mahdenko2020ks13@student.karazin.ua>
2023-05-10 20:34:16 +0300

    LR 9

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'nokogiri'
 
 gem 'prawn'
 
+gem 'activeadmin'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.3"
 
---
+++ app/admin/books.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Book do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :description, :isbn, :publication_year, :library_id, :readers_card_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :description, :isbn, :publication_year, :library_id, :readers_card_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
